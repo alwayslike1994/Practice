7019182a1b663f57e2e06546f4985f53d7faed0f
312047391 <312047391@qq.com>
2018-05-28 20:30:30 +0800

    。。

---
+++ src/TreeQuestion/TreeNode.java
@@ -0,0 +1,12 @@
+package TreeQuestion;
+
+public class TreeNode {
+    int value;
+    TreeNode left = null;
+    TreeNode right = null;
+    TreeNode parent = null;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
